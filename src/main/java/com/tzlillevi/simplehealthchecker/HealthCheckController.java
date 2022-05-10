bc29d44a1a5cf14fa67d0decd1b6b0ae47bb8df8
TzlilLevi <57330791+TzlilLevi@users.noreply.github.com>
2022-05-10 17:29:57 +0300

    health check to single target

--- src/main/java/com/tzlillevi/simplehealthchecker/HealthCheckController.java
+++ src/main/java/com/tzlillevi/simplehealthchecker/HealthCheckController.java
@@ -1,8 +1,12 @@
 package com.tzlillevi.simplehealthchecker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.Random;
@@ -12,12 +16,24 @@ import java.util.Random;
 
 //@RequestMapping
 public class HealthCheckController {
-
+    @Value("${healthcheck.target}")
+    private String target;
     @GetMapping("/health")
     public HealthResponse getHealth(HttpServletResponse response) {
-        var random = new Random();
-        var checkHealth = random.nextBoolean();
-        if (!checkHealth) {
+
+        final String uri= target;
+        RestTemplate restTemplate = new RestTemplate();
+        boolean isHealthy;
+        try {
+            ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+            isHealthy = result.getStatusCode().is2xxSuccessful();
+        }
+        catch(Exception e){
+            isHealthy = false;
+        }
+
+
+        if (!isHealthy) {
             response.setStatus(503);
             System.out.println("ERROR: 503");
             return new HealthResponse("BAD");
